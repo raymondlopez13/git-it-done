12c37cc9047672a04cc94dc2a24829bdfb2e617b
Raymond Lopez <raymondlopez13@yahoo.com>
2021-01-15 09:58:45 -0600

    ADdded buttons to HTML that can search for featured repos by language type

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,6 @@
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector('#language-buttons');
 
 
 var getUserRepos = function(user) {
@@ -89,3 +90,28 @@ var displayRepos = function(repos, searchTerm) {
         repoContainerEl.appendChild(repoEl);
     }
 };
+
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+            });
+        } else {
+            alert("Error" + response.statusText);
+        }
+    })
+};
+
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute('data-language');
+    console.log(language);
+    if (language) {
+        getFeaturedRepos(language);
+
+        repoContainerEl.textContent = '';
+    }
+};
+
+languageButtonsEl.addEventListener('click', buttonClickHandler);
