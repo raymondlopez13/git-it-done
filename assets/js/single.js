042e23604e987786c7ea11310670d55b00a660b7
Raymond Lopez <raymondlopez13@yahoo.com>
2021-01-14 19:11:02 -0600

    Added limit to number of issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector('#issues-container');
+var limitWarningEl = document.querySelector('#limit-warning');
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -8,6 +9,10 @@ var getRepoIssues = function(repo) {
         if (response.ok) {
             response.json().then(function(data) {
                 displayIssues(data);
+
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         } else {
             alert("There was a problem with your request!");
@@ -45,4 +50,14 @@ var displayIssues = function(issues) {
     }
 };
 
-getRepoIssues('raymondlopez13/git-it-done');
+var displayWarning = function(repo) {
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+    var linkEl = document.createElement('a');
+    linkEl.textContent = "See More Issues on Github.com";
+    linkEl.setAttribute('href', "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute('target', '_blank');
+
+    limitWarningEl.appendChild(linkEl);
+};   
+
+getRepoIssues('facebook/react');
